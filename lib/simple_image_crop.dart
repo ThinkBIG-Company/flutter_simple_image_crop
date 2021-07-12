57114d7b520a4efbd7df60f827a2e42ecb5fa027
ThinkBIG-Company <developers@think-big-company.com>
2021-07-12 13:21:40 +0200

    2.0.0

--- lib/simple_image_crop.dart
+++ lib/simple_image_crop.dart
@@ -9,9 +9,8 @@ import 'dart:ui';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 part 'src/simpleCrop.dart';
-
 part 'src/simpleImageCrop.dart';
